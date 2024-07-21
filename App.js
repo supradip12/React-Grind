6fa9ec00e3026ebafe747504eccb9e146403d90b
Supra <supradipdey1@gmail.com>
2024-07-21 12:22:41 +0530

    2nd Epti

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h2", { id: "loda" }, [
   React.createElement("h3", { id: "loda1" }, "Hlloo load1"),
   React.createElement("h3", { id: "loda2" }, "Hlloo load2"),
