6a3d9300c7e1e183d48ea431541e25f56f38ce78
Supra <supradipdey1@gmail.com>
2024-07-21 10:43:56 +0530

    React started

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h2", { id: "loda" }, [
+  React.createElement("h3", { id: "loda1" }, "Hlloo load1"),
+  React.createElement("h3", { id: "loda2" }, "Hlloo load2"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
