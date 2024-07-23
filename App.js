a62615d5135f278b882a73fc4740254ed89179e6
Supra <supradipdey1@gmail.com>
2024-07-23 20:43:41 +0530

    2nd chsnge

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const heading = React.createElement("h2", { id: "loda" }, [
-  React.createElement("h3", { id: "loda1" }, "Hlloo load1"),
-  React.createElement("h3", { id: "loda2" }, "Hlloo load2"),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
